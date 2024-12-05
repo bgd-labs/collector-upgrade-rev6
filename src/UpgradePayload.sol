d9016435dc9e3ffd211f648fee111ebb5ab3f7fe
Lukas <lukasstrassel@googlemail.com>
2024-12-05 16:27:55 +0100

    fix: add permissions

--- src/UpgradePayload.sol
+++ src/UpgradePayload.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import {ITransparentUpgradeableProxy, ProxyAdmin} from "solidity-utils/contracts/transparent-proxy/TransparentUpgradeableProxy.sol";
-import {ICollector} from "aave-v3-origin/contracts/treasury/Collector.sol";
+import {IAccessControl} from "aave-v3-origin/contracts/dependencies/openzeppelin/contracts/IAccessControl.sol";
+import {ICollector} from "aave-v3-origin/contracts/treasury/ICollector.sol";
 
 contract UpgradePayload {
     address public immutable PROXY_ADMIN;
@@ -15,10 +16,16 @@ contract UpgradePayload {
     }
 
     function execute() external {
+        // upgrade collector implementation with custom initialize to align storage
         ProxyAdmin(PROXY_ADMIN).upgradeAndCall(
             ITransparentUpgradeableProxy(COLLECTOR),
             COLLECTOR_IMPL,
             abi.encodeWithSelector(ICollector.initialize.selector, 0)
+        );
+        // grant funds admin permissions to the executor
+        IAccessControl(ICollector(COLLECTOR).ACL_MANAGER()).grantRole(
+            ICollector(COLLECTOR).FUNDS_ADMIN_ROLE(),
+            address(this)
         );
     }
 }
