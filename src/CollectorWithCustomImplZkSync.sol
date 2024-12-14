996980466d17423bef842800efbd42e9f5a8bfd7
Harsh Pandey <harshsatishpandey@gmail.com>
2024-12-14 17:42:58 +0530

    chore: fix lint and zksync configs

--- src/CollectorWithCustomImplZkSync.sol
+++ src/CollectorWithCustomImplZkSync.sol
@@ -10,17 +10,18 @@ import {Collector, ICollector} from "aave-v3-origin/contracts/treasury/Collector
  * - storage slot 51 is reset to 0
  * - storage slot 52 is set to 1 (which is the default state of the reentrancy guard)
  * @author BGD Labs
- **/
+ *
+ */
 contract CollectorWithCustomImplZkSync is Collector {
-    constructor(address aclManager) Collector(aclManager) {
-        // intentionally left empty
-    }
+  constructor(address aclManager) Collector(aclManager) {
+    // intentionally left empty
+  }
 
-    /// @inheritdoc ICollector
-    function initialize(uint256) external virtual override initializer {
-        assembly {
-            sstore(53, 100000) // this slot was _fundsAdmin, but is now _nextStreamId
-            sstore(54, 0) // this slot was _nextStreamId, but is now _streams
-        }
+  /// @inheritdoc ICollector
+  function initialize(uint256) external virtual override initializer {
+    assembly {
+      sstore(53, 100000) // this slot was _fundsAdmin, but is now _nextStreamId
+      sstore(54, 0) // this slot was _nextStreamId, but is now _streams
     }
+  }
 }
--- test/Mainnet.t.sol
+++ test/Mainnet.t.sol
@@ -6,13 +6,13 @@ import {UpgradeTest} from "./UpgradeTest.sol";
 import {DeploymentLibrary} from "../script/Deploy.s.sol";
 
 contract MainnetTest is UpgradeTest {
-    constructor() UpgradeTest("mainnet", 21336615) {}
+  constructor() UpgradeTest("mainnet", 21336615) {}
 
-    function _getPayload() internal virtual override returns (address) {
-        return DeploymentLibrary.deployMainnet();
-    }
+  function _getPayload() internal virtual override returns (address) {
+    return DeploymentLibrary.deployMainnet();
+  }
 
-    function _getPool() internal virtual override returns (IPool) {
-        return AaveV3Ethereum.POOL;
-    }
+  function _getPool() internal virtual override returns (IPool) {
+    return AaveV3Ethereum.POOL;
+  }
 }
